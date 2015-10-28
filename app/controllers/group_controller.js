8485e7c7cdc3fc95215084b6578f44769b6a80e6
John <tigertech63@gmail.com>
2015-10-28 14:53:50 -0700

    sorting is working after modifying a group

--- app/controllers/group_controller.js
+++ app/controllers/group_controller.js
@@ -21,8 +21,8 @@ exports.index = function (req,res){
 		groups.fetch({id: req.body.id})
 			 .then(function (data) {
 				res.render('groups/create', {
-					title: 'Your Groups', 
-					userId: req.user.get('id'), 
+					title: 'Your Groups',
+					userId: req.user.get('id'),
 					username: req.user.get('username')
 				});
 			})
@@ -30,7 +30,7 @@ exports.index = function (req,res){
 			console.error(error.stack);
 			res.redirect('/error');
 		})
-		
+
 	}else {
 			res.render('users/signin', {title: 'Sign Up'});
 	}
@@ -73,12 +73,14 @@ exports.create = function (req,res){
 //Show
 exports.show = function (req,res) {
 	var groups = Groups;
-	groups.fetch()
+	groups
+	.query('orderBy', 'id', 'asc')
+	.fetch()
 	.then(function (data) {
 		res.render('groups/groups', {
-			title: 'Current Groups', 
-			groups: data.toJSON(), 
-			userId: req.user.get('id'), 
+			title: 'Current Groups',
+			groups: data.toJSON(),
+			userId: req.user.get('id'),
 			username: req.user.get('username')
 		})
 	})
@@ -100,15 +102,15 @@ exports.editShow = function (req,res) {
 	.then(function (group) {
 		if(req.isAuthenticated()) {
 			res.render('groups/edit', {
-				title: 'Edit Group', 
-				group: group.toJSON(), 
-				userId: req.user.get('id'), 
+				title: 'Edit Group',
+				group: group.toJSON(),
+				userId: req.user.get('id'),
 				username: req.user.get('username')
 			})
 		} else {
 			res.render('users/signin', {
-				title: 'Sign Up', 
-				userId: req.user.get('id'), 
+				title: 'Sign Up',
+				userId: req.user.get('id'),
 				username: req.user.get('username')
 			});
 		}
@@ -168,8 +170,8 @@ exports.destroy = function (req,res) {
 		})
 	} else {
 		res.render('users/signin', {
-			title: 'Sign In', 
-			userId: req.user.get('id'), 
+			title: 'Sign In',
+			userId: req.user.get('id'),
 			username: req.user.get('username')
 		});
 	}
